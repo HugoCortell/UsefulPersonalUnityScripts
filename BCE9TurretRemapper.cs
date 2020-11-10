a9c61b3bddbefef64300f7d20ab0a34db35d0cc0
HugoCortell <hugo@cortell.net>
2020-11-10 04:53:21 +0100

    Add files via upload

---
+++ BCE9TurretRemapper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BCE9TurretRemapper : MonoBehaviour
+{
+    // ASSUMPTIONS:
+    // * THIS IS ALWAYS GOING TO BE ON THE X AXIS, MIDPOINT IS ZERO.
+
+    // Actual gun elevations
+    [SerializeField] private float _MinimunElevation = 10;
+    [SerializeField] private float _MaximunElevation = -20;
+
+    // Rotator's hinge maximuns
+    [SerializeField] private float _RotationMinimun = 180;
+    [SerializeField] private float _RotationMaximun = -180;
+
+    public void RemapTurretRotation(float _RotatorValue) // Is dynamic - called via event.
+    {
+        transform.rotation = Quaternion.Euler((_RotatorValue - _RotationMinimun) / (_RotationMaximun - _RotationMinimun) * (_MaximunElevation - _MinimunElevation) + _MinimunElevation, 0, 0);
+    } // NOTE: transform.rotate can go to hell
+}
