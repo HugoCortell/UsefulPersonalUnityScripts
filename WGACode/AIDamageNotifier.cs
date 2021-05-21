7ca7e170edbadb13d93b3e20ab3e598db0796d6e
HugoCortell <hugo@cortell.net>
2021-05-21 11:03:14 +0200

    Add files via upload

---
+++ WGACode/AIDamageNotifier.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using HurricaneVR.Framework.Components;
+
+public class AIDamageNotifier : HVRDamageHandlerBase
+{
+    [Header("Statistics")]
+    [SerializeField] private int _PartSection = 2; // Head, Torso, Legs (hands? Unused.)
+    [SerializeField] private float _PartHealth = 100f;
+    [SerializeField] private ParticleSystem[] _ReactionFX;
+
+    [Header("Agent Refs")]
+    [SerializeField] private AICore _Core;
+
+
+    public override void TakeDamage(float damage)
+    {
+        _PartHealth -= damage;
+
+        int SelectedReaction = 0;
+        if (_ReactionFX.Length != 1) {SelectedReaction = Random.Range(0, _ReactionFX.Length);}
+        _ReactionFX[SelectedReaction].gameObject.transform.position = gameObject.transform.position; // FX is separate object because this gets destroyed upon death
+        _ReactionFX[SelectedReaction].gameObject.transform.rotation = Quaternion.LookRotation(_Core._PlayerTorso.transform.position - gameObject.transform.position);
+        _ReactionFX[SelectedReaction].Play();
+
+        if (_PartHealth <= 0)
+        {
+            _Core.Death(_PartSection); // grommit mug
+            return;
+        }
+
+        _Core.ReactToDamage();
+    }
+}
---
+++ WGACode/BloodPoolBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class BloodPoolBehaviour : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _Sprite;
+    [SerializeField] private Color[] _Colors; // Rot, Transparent
+
+    private void Awake()
+    {
+        Invoke("InitiateExpansion", Random.Range(0.25f, 2.5f));
+    }
+
+    private void InitiateExpansion()
+    {
+        float finalsize = Random.Range(0.25f, 1.15f);
+        float timeuntilfullsize = Random.Range(3f, 12f);
+
+        // Begin to grow pool
+        gameObject.transform.DOScale(new Vector3(finalsize, finalsize, finalsize), timeuntilfullsize);
+        Invoke("BeginRotting", timeuntilfullsize + Random.Range(2f, 10f));
+    }
+
+    private void BeginRotting()
+    {
+        float timeuntilrottingends = Random.Range(12f, 300f);
+
+        _Sprite.DOColor(_Colors[0], timeuntilrottingends);
+        Invoke("CleanUpBlood", timeuntilrottingends);
+    }
+
+    private void CleanUpBlood()
+    {
+        float timeuntilfullygone = Random.Range(6f, 30f);
+
+        _Sprite.DOColor(_Colors[1], timeuntilfullygone);
+        Destroy(gameObject, timeuntilfullygone);
+    }
+}
